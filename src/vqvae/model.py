7c5921a928302c5424cfce63badc7081a9f46b78
djordjened92 <djordjened92@gmail.com>
2021-05-15 01:50:04 +0200

    Use exponential moving average instead of codebook loss

--- src/vqvae/model.py
+++ src/vqvae/model.py
@@ -22,23 +22,37 @@ class Quantize(nn.Module):
 
     def __init__(self, size, code_dim):
         super().__init__()
-        self.embedding = nn.Embedding(size, code_dim)
-        self.embedding.weight.data.uniform_(-1./size,1./size)
+        self.register_buffer('embeddings', torch.randn(size, code_dim))
+        self.register_buffer('N', torch.zeros(size))
+        self.register_buffer('z_avg', self.embeddings.data.clone())
 
         self.code_dim = code_dim
         self.size = size
 
     def forward(self, z):
         b, c, h, w = z.shape
-        weight = self.embedding.weight
 
         flat_inputs = z.permute(0, 2, 3, 1).contiguous().view(-1, self.code_dim)
         
         distances = ((flat_inputs.repeat(1, self.size).view(-1, self.size, self.code_dim) \
-                     - weight) ** 2).sum(dim=-1)
-        encoding_indices = torch.min(distances, dim=-1)[1]
+                     - self.embeddings) ** 2).sum(dim=-1)
+        encoding_indices = torch.argmin(distances, dim=-1)
+        encode_onehot = F.one_hot(encoding_indices, self.size).type_as(flat_inputs)
         encoding_indices = encoding_indices.view(b, h, w)
-        quantized = self.embedding(encoding_indices).permute(0, 3, 1, 2).contiguous()
+        quantized = F.embedding(encoding_indices, self.embeddings).permute(0, 3, 1, 2).contiguous()
+
+        # EMA codebook update
+        if self.training:
+            n_total = encode_onehot.sum(dim=0)
+            encode_sum = flat_inputs.t() @ encode_onehot
+
+            self.N.data.mul_(0.99).add_(n_total, alpha=0.01)
+            self.z_avg.data.mul_(0.99).add_(encode_sum.t(), alpha=0.01)
+
+            n = self.N.sum()
+            weights = (self.N + 1e-7) / (n + self.size * 1e-7) * n
+            encode_normalized = self.z_avg / weights.unsqueeze(1)
+            self.embeddings.data.copy_(encode_normalized)
 
         return quantized, (quantized - z).detach() + z, encoding_indices
 
@@ -112,9 +126,8 @@ class VectorQuantizedVAE(nn.Module):
         e, e_st, _ = self.codebook(z)
         x_tilde = self.decoder(e_st)
 
-        diff1 = torch.mean((z - e.detach()) ** 2)
-        diff2 = torch.mean((e - z.detach()) ** 2)
-        return x_tilde, 0.25 * diff1 + diff2
+        commitment_loss = 0.25 * torch.mean((z - e) ** 2)
+        return x_tilde, commitment_loss
 
     def loss(self, x):
         x = 2 * x - 1
