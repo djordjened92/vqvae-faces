Fix PixelSNAIL attention masking inf -> -1e8 to avoid nans in Softmax backward computation

src/combined/combined_model.py
@@ -5,17 +5,32 @@ import torch.nn.functional as F
 from torch import nn
 from collections import OrderedDict
 from src.vqvae.model import VectorQuantizedVAE
-from src.pixelCNN.model import PixelCNN, GatedPixelCNN
+from src.pixelCNN.model import PixelCNN, GatedPixelCNN, PixelSNAIL
 
 class VQVAE_PixelCNN(nn.Module):
-    def __init__(self, code_dim, code_size, latent_shape, pixelCNN_gated=False, pixelCNN_n_layers=7):
+    def __init__(self, code_dim, code_size, latent_shape, pixelCNN_type, pixelCNN_n_layers=7):
         super().__init__()
         self.vqvae = VectorQuantizedVAE(code_dim, code_size).cuda()
 
-        if pixelCNN_gated:
-            self.pixelCNN = GatedPixelCNN(latent_shape, size=code_size, n_layers=pixelCNN_n_layers).cuda()
+        if pixelCNN_type in ['pixelCNN', 'gatedPixelCNN']:
+            pixelCNN = pixelCNN_types[pixelCNN_type](
+                input_shape=(input_h, input_w),
+                size=code_size,
+                n_layers=n_layers
+            ).cuda()
+        elif pixelCNN_type == 'pixelSNAIL':
+            pixelCNN = PixelSNAIL(
+                in_channels=1,
+                out_channels=code_size,
+                n_channels=code_size // 2,
+                n_pixel_snail_blocks=n_layers,
+                n_residual_blocks=2,
+                attention_value_channels=code_size // 2,  # n_channels / 2
+                attention_key_channels=16
+            ).cuda()
         else:
-            self.pixelCNN = PixelCNN(latent_shape, size=code_size, n_layers=pixelCNN_n_layers).cuda()
+            print(f'Incorrect pixelCNN model type')
+            exit(1)
     
     def forward(self, x):
         # Encoder and Quantizier
@@ -25,19 +40,14 @@ class VQVAE_PixelCNN(nn.Module):
         # PixelCNN infernece
         e_ind = e_ind.unsqueeze(dim=1)
         e_ind_ar = self.pixelCNN(e_ind.detach())
-        # e_ind_am = torch.argmax(F.softmax(e_ind_ar, dim=1), dim=1).long()
-        # e = self.vqvae.codebook.embedding(e_ind_am).permute(0, 3, 1, 2).contiguous()
-        # e_st = ((e if epoch > 20 else e_q) - z).detach() + z
         x_tilde = self.vqvae.decoder(e_st)
-
-        diff1 = torch.mean((z - e_q.detach()) ** 2)
-        diff2 = torch.mean((e_q - z.detach()) ** 2)
-        return x_tilde, diff1 + diff2, e_ind, e_ind_ar
+        commitment_loss = 0.15 * torch.mean((z - e_q) ** 2)
+        return x_tilde, commitment_loss, e_ind, e_ind_ar
     
     def loss(self, x):
         x = 2 * x - 1
-        x_tilde, diff, e_ind, e_ind_ar = self(x)
+        x_tilde, commitment_loss, e_ind, e_ind_ar = self(x)
         recon_loss = F.mse_loss(x_tilde, x)
         ll = F.cross_entropy(e_ind_ar, e_ind.squeeze())
-        loss = recon_loss + diff + 8*ll#(1e-2 if epoch > 30 else 0) * ll
-        return OrderedDict(loss=loss, recon_loss=recon_loss, reg_loss=diff, log_likelihood=ll)
+        loss = recon_loss + commitment_loss + 8*ll#(1e-2 if epoch > 30 else 0) * ll
+        return OrderedDict(loss=loss, recon_loss=recon_loss, reg_loss=commitment_loss, log_likelihood=ll)

src/pixelCNN/main.py
@@ -12,6 +12,9 @@ from src.training import train_epochs
 from src.pixelCNN.model import PixelCNN, GatedPixelCNN, PixelSNAIL
 from src.vqvae.model import VectorQuantizedVAE
 
+# Try to catch cause of anomalies like nan loss
+torch.autograd.set_detect_anomaly(True)
+
 def main(args):
     # Load pixelcnn configuration
     with open(args.config_path, 'r') as f:
@@ -102,12 +105,12 @@ def main(args):
         pixelCNN = PixelSNAIL(
             in_channels=1,
             out_channels=code_size,
-            n_channels=code_size,
+            n_channels=code_size // 2,
             n_pixel_snail_blocks=n_layers,
             n_residual_blocks=2,
             attention_value_channels=code_size // 2,  # n_channels / 2
             attention_key_channels=16
-        )
+        ).cuda()
     else:
         print(f'Incorrect pixelCNN model type')
         exit(1)

src/pixelCNN/model/gatedPixelCNN.py
@@ -81,7 +81,7 @@ class GatedPixelCNN(nn.Module):
     self.in_conv = MaskConv2d('A', 1, size // 2, 5, padding=2)
     model = []
     for _ in range(n_layers - 2):
-      model.extend([nn.ReLU(), nn.Dropout(0.3), StackLayerNorm(size // 2), GatedConv2d('B', size // 2, size // 2, 3, padding=1)])
+      model.extend([nn.ReLU(), nn.Dropout(0.2), StackLayerNorm(size // 2), GatedConv2d('B', size // 2, size // 2, 3, padding=1)])
     model.extend([nn.ReLU(), StackLayerNorm(size // 2)])
     self.out_conv = MaskConv2d('B', size // 2, size, 5, padding=2)
     self.net = nn.Sequential(*model)
@@ -94,7 +94,7 @@ class GatedPixelCNN(nn.Module):
     return out
   
   def loss(self, x):
-    return OrderedDict(loss=F.cross_entropy(self(x), x.squeeze()))
+    return OrderedDict(loss=F.cross_entropy(self(x), x.squeeze(1)))
 
   def sample(self, n):
     samples = torch.zeros(n, *self.input_shape).long().cuda()

src/pixelCNN/model/pixelCNN.py
@@ -76,7 +76,7 @@ class PixelCNN(nn.Module):
         return out
 
     def loss(self, x):
-        return OrderedDict(loss=F.cross_entropy(self(x), x.squeeze()))
+        return OrderedDict(loss=F.cross_entropy(self(x), x.squeeze(1)))
 
     def sample(self, n):
         samples = torch.zeros(n, *self.input_shape).long().cuda()

src/pixelCNN/model/pixelSNAIL/causal_attention.py
@@ -116,8 +116,8 @@ class CausalAttention(nn.Module):
             .to(next(self.parameters()).device)
         )
         attn = (q @ k.transpose(2, 3)) / np.sqrt(k.shape[-1])
-        attn = attn.masked_fill(mask == 0, -np.inf)
-        attn = F.softmax(attn, dim=-1).masked_fill(mask == 0, 0)
+        attn = attn.masked_fill(mask == 0, -1e8)
+        attn = F.softmax(attn, dim=-1)
 
         # Attent to output for each head, stack, and project.
         out = (attn @ v).transpose(2, 3).contiguous().view(n, -1, h, w)

src/pixelCNN/model/pixelSNAIL/pixelSNAIL.py
@@ -2,7 +2,7 @@ import torch
 from collections import OrderedDict
 from torch import nn
 from torch.nn import functional as F
-from .causal_attention import CausalAttention
+from .causal_attention import CausalAttention, image_positional_encoding
 from .base import AutoregressiveModel
 
 def _elu_conv_elu(conv, x):
@@ -77,7 +77,7 @@ class ResidualBlock(nn.Module):
             kernel_size=2,
             padding=1,
         )
-        self._activation = GatedActivation(activation_fn=nn.Identity())
+        self._activation = GatedActivation()
 
     def forward(self, x):
         _, c, h, w = x.shape
@@ -210,6 +210,7 @@ class PixelSNAIL(AutoregressiveModel):
         )
 
     def forward(self, x):
+        # x = x.float()
         input_img = x
         x = self._input(x)
         for block in self._pixel_snail_blocks:
@@ -217,75 +218,11 @@ class PixelSNAIL(AutoregressiveModel):
         return self._output(x)
     
     def loss(self, x):
-        return OrderedDict(loss=F.cross_entropy(self(x), x.squeeze()))
+        return OrderedDict(loss=F.cross_entropy(self(x), x.squeeze(1)))
 
-
-def reproduce(
-    n_epochs=457,
-    batch_size=128,
-    log_dir="/tmp/run",
-    n_gpus=1,
-    device_id=0,
-    debug_loader=None,
-):
-    """Training script with defaults to reproduce results.
-
-    The code inside this function is self contained and can be used as a top level
-    training script, e.g. by copy/pasting it into a Jupyter notebook.
-
-    Args:
-        n_epochs: Number of epochs to train for.
-        batch_size: Batch size to use for training and evaluation.
-        log_dir: Directory where to log trainer state and TensorBoard summaries.
-        n_gpus: Number of GPUs to use for training the model. If 0, uses CPU.
-        device_id: The device_id of the current GPU when training on multiple GPUs.
-        debug_loader: Debug DataLoader which replaces the default training and
-            evaluation loaders if not 'None'. Do not use unless you're writing unit
-            tests.
-    """
-    from torch import optim
-    from torch.nn import functional as F
-    from torch.optim import lr_scheduler
-
-    # from pytorch_generative import datasets
-    # from pytorch_generative import models
-    # from pytorch_generative import trainer
-
-    # train_loader, test_loader = debug_loader, debug_loader
-    # if train_loader is None:
-    #     train_loader, test_loader = datasets.get_mnist_loaders(
-    #         batch_size, dynamically_binarize=True
-    #     )
-
-    model = PixelSNAIL(
-        in_channels=1,
-        out_channels=1,
-        n_channels=64,
-        n_pixel_snail_blocks=8,
-        n_residual_blocks=2,
-        attention_value_channels=32,  # n_channels / 2
-        attention_key_channels=4,  # attention_value_channels / 8
-    )
-    # optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    # scheduler = lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lambda _: 0.999977)
-
-    # def loss_fn(x, _, preds):
+    # def loss(self, x):
+    #     preds = self(x)
     #     batch_size = x.shape[0]
     #     x, preds = x.view((batch_size, -1)), preds.view((batch_size, -1))
     #     loss = F.binary_cross_entropy_with_logits(preds, x, reduction="none")
     #     return loss.sum(dim=1).mean()
-
-    # trainer = trainer.Trainer(
-    #     model=model,
-    #     loss_fn=loss_fn,
-    #     optimizer=optimizer,
-    #     train_loader=train_loader,
-    #     eval_loader=test_loader,
-    #     lr_scheduler=scheduler,
-    #     log_dir=log_dir,
-    #     n_gpus=n_gpus,
-    #     device_id=device_id,
-    # )
-    # trainer.interleaved_train_and_eval(n_epochs)
-if __name__ == '__main__':
-    reproduce()